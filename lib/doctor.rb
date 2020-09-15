d8f5e9cc71e5cbe8acb2ae65708a10401ef8429d
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-15 01:24:49 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,17 +15,14 @@ class Doctor
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
-    
-
-  def new_appointment(date, patient)
-   Appointment.new(patient, date, self)
-    
-  end
   
   def patients
     appointments.map {|appointment| appointment.patient}
   end
-
+  
+  def new_appointment(date, patient)
+   Appointment.new(patient, date, self)
+  end
   
  
 end
