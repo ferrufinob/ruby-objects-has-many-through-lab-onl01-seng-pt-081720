0da4390324cf67ee97397239332ef7fd2f6fd58f
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-15 01:28:53 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -20,7 +20,7 @@ class Patient
    end 
   
    def doctors
-    appointments.collect {|appointment| appointment.doctor}
+    Appointment.collect {|appointment| appointment.doctor}
   end
   
   def new_appointment(date, doctor)
