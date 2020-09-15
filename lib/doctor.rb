68b74c75118624c8f1c6ec33b2f5ccae2ad4685d
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-15 01:37:04 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
   
   def patients
-   self.appointments.collect do |appointment| appointment.patient
+   self.appointments.collect do |appointment| appointment.Patient
   end
 end
   
